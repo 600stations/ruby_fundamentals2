47c966ec856cbe731a5f5b92f7dc4651f1a2e005
ChrisP <600steps@gmail.com>
2016-08-09 15:38:24 -0400

    deleted second cohort hash.delete(key-no quotes)

--- exercise7.rb
+++ exercise7.rb
@@ -3,9 +3,16 @@ students = {
   :cohort2 => 42,
   :cohort3 => 22
 }
-def display_student(s)
+students[:cohort4] = 43 # adds a key and value to hash
+
+=begin
+def display_student(s) #method to iterate through any hash
   s.each do|k,v|
-    puts " #{k}: has #{v} students"
+    puts " #{k}: has #{(v*1.05).round}" #increase class size by 5% and round down
   end
 end
-display_student(students)
+display_student(students) #calls my method with my hash as parameter
+=end
+
+students.delete(:cohort2)# delete second key value pair in hash
+puts students
