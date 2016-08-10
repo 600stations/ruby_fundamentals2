c1f94fa6ed64ad830aa0d80e01da1cb790c16dc3
ChrisP <600steps@gmail.com>
2016-08-10 14:12:18 -0400

    refined sum statement of total students

--- exercise6.rb
+++ exercise6.rb
@@ -1,14 +1,25 @@
 grocery_list = ["spinach", "bananas", "apples", "lettuce"]
-grocery_list.push("onions", "coriander")
+grocery_list.push("onions", "coriander")#adds items to list
 grocery_list.each do |x|
-  puts "* #{x}"
+  puts "* #{x}"#puts asterisk before item on iterated list
   end
 puts grocery_list.length # gives number of items on list
 if grocery_list.include?("bananas")# checks if bananas on list
   puts "You don't need to pick up bananas today!"
+else
+  puts "Pick up some bananas!"
 end
+#--------add method that adds items to list
+def add_to_list(x)
+
+  #if grocery_list(x)
+  #puts "#{item} is on your list"
+  #else
+  #grocery_list.push(item)
+  #end
+#add_to_list(rice) checks for then adds item to grocery_list  
 puts grocery_list[1] # displays the second item on my list
 puts grocery_list.sort # sorts my list
-puts
 grocery_list.delete_at(0)#deletes spinach from my list
 puts grocery_list
+#add to shopping list method-add rice----------------------!!!
--- exercise7.rb
+++ exercise7.rb
@@ -9,14 +9,15 @@ students[:cohort4] = 43 # adds a key and value to hash
 def display_student(s) #method to iterate through any hash
   sum = 0 # declares initial value of sum
   s.each do|k,v|
-    puts " #{k}: has #{(v*1.05).round}" #increase class size by 5% and round down
+    puts " #{k}: has #{(v*1.05).round} students" #increase class size by 5% and round down
 
     sum +=v #counter for sum of values
   end
-  puts sum # puts sum of values
+  puts "#{sum} total students"# puts sum of values
 end
 
 display_student(students) #calls my method with my hash as parameter
+puts students.keys# also displays all cohort names i.e. keys
 
 
 students.delete(:cohort2)# delete second key value pair in hash-note: no quotes for key
